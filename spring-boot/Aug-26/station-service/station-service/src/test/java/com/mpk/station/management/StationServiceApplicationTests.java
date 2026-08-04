bafa3fe8aa60ed4bf72a7d86c2da46576d65dfa3
pavan kiran maddela <33048163+pavankiranmaddela@users.noreply.github.com>
2026-08-04 09:50:00 +0530

    Class1: spring boot project creation

---
+++ spring-boot/Aug-26/station-service/station-service/src/test/java/com/mpk/station/management/StationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpk.station.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
